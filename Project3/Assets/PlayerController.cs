0aeca2107febe19aff7c716c8268a4726a965378
Quartz Bushnell <shockedquartz0@gmail.com>
2023-02-07 19:41:38 -0500

    adds jumping

--- Project3/Assets/PlayerController.cs
+++ Project3/Assets/PlayerController.cs
@@ -4,22 +4,27 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform camera;
-
     [SerializeField] float speed;
+    [SerializeField] float jumpHeight;
     [SerializeField] float sensitivity;
 
     Vector3 prevMousePos;
     Vector3 mousePos;
 
+    Vector3 jumpVector;
+    bool jumpActive = true;
+
+    Rigidbody rb;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         mousePos = Input.mousePosition;
+        jumpVector = new Vector3(0, jumpHeight, 0);
     }
 
     private void Update()
     {
-
         prevMousePos = mousePos;
         mousePos = Input.mousePosition;
         transform.Rotate(new Vector3(0, (mousePos.x - prevMousePos.x) * sensitivity, 0));
@@ -43,5 +48,17 @@ public class PlayerController : MonoBehaviour
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
         }
+
+        if (Input.GetKey(KeyCode.Space) && jumpActive)
+        {
+            rb.AddForce(jumpVector, ForceMode.Impulse);
+            jumpActive = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.y, 0));
+        jumpActive = true;
     }
 }
